e5f967a9d2a3ad1c6ffa9278813533c3035bcda8
Syed Muhammed Bilal <intergo878@gmail.com>
2020-08-12 22:22:41 +0500

    Add files via upload

---
+++ client/src/App.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+
+import logo from './logo.png';
+import './App.css';
+import Launches from './components/Launches';
+import Launch from './components/Launch';
+
+const client = new ApolloClient({
+  uri: "http://localhost:5000/graphql"
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <Router>
+        <div className="container">
+          <img
+            src={logo}
+            alt="spaceX"
+            style={{ width: 300, display: "block", margin: "auto" }}
+          />
+          <Route exact path="/" component={Launches} />
+          <Route exact path="/launch/:flight_number" component={Launch} />
+        </div>
+      </Router>
+    </ApolloProvider>
+  );
+}
+
+export default App;
---
+++ client/src/components/LaunchItem.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import classNames from 'classnames';
+import Moment from 'react-moment';
+import { Link } from 'react-router-dom'
+
+type Props = {
+  launch: any
+}
+
+const LaunchItem: React.FC<Props> = ({ launch: { flight_number, mission_name, launch_date_local, launch_success, launch_site } }) => {
+  //console.log(launch);
+  return (
+    <div className="card card-body mb-3">
+      <div className="row">
+        <div className="col-md-9">
+          <h4> Mission:
+            <span className={classNames({
+              'text-success': launch_success,
+              'text-danger': !launch_success
+            })}>
+              {mission_name}
+            </span>
+          </h4>
+          <p> Date: <Moment format="YYYY-MM-DD HH:mm">{launch_date_local}</Moment> </p>
+          <p> Launch Site: {launch_site.site_name} </p>
+        </div>
+        <div className="col-md-3">
+            <Link to={`/launch/${flight_number}`} className="btn btn-secondary"> Launch Details </Link>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default LaunchItem
