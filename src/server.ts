d0cc783e52d68d28d8263cc44f5d5544c4941356
Syed Muhammed Bilal <intergo878@gmail.com>
2020-08-18 23:03:17 +0500

    Add files via upload

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const path = require('path');
 const { graphqlHTTP } = require('express-graphql');
 const schema = require('./schema');
 
